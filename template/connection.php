1249990238169c7a6f391cb8c4eb65f71e93aecc
HP\Niklas <niklas.saarikuru@gmail.com>
2016-11-09 11:57:34 +0200

    Added .gitignore

--- template/connection.php
+++
@@ -1,8 +0,0 @@
-	<?php
-			$connect = new PDO("mysql:host=localhost; dbname=bulletproof", "pretzel_admin","admin");
-			$query = "SELECT id, name, img , price, description FROM products ORDER BY id ASC";
-
-			$kysely = $connect->prepare($query);
-			$kysely->execute();
-
-			?>
